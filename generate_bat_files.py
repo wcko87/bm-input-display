2cc12366605e9b552c1276c2c8cfe0a4eb617400
wcko87 <wcko87@gmail.com>
2020-01-01 16:40:01 -0500

    update generate_bat_files.py

--- generate_bat_files.py
+++ generate_bat_files.py
@@ -6,4 +6,5 @@ for file in os.listdir('configs'):
     if not file.startswith(PREFIX) or not file.endswith(POSTFIX): continue
     config_name = file[len(PREFIX):-len(POSTFIX)]
     with open('inputdisplay_%s.bat' % config_name, 'w+') as f:
-        f.write(r'bin\inputdisplay config_%s.txt' % config_name)
+        f.write(r'bin\inputdisplay configs\config_%s.txt' % config_name)
+        f.write('\npause')
