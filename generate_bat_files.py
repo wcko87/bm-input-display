4bbf209bddbd31ea8842659365a187957eaf81a5
wcko87 <wcko87@gmail.com>
2020-01-01 16:27:30 -0500

    add script to generate bat files during build

---
+++ generate_bat_files.py
@@ -0,0 +1,9 @@
+import os
+PREFIX = 'config_'
+POSTFIX = '.txt'
+
+for file in os.listdir('configs'):
+    if not file.startswith(PREFIX) or not file.endswith(POSTFIX): continue
+    config_name = file[len(PREFIX):-len(POSTFIX)]
+    with open('inputdisplay_%s.bat' % config_name, 'w+') as f:
+        f.write(r'bin\inputdisplay config_%s.txt' % config_name)
